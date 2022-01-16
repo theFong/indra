fa923c2849c79cd5b024bb1ca01b7f9aeb6edfc7
Alec Fong <alecfong1@gmail.com>
2022-01-16 14:45:50 -0800

    add to tech demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 
-import ReactFlow, { removeElements, addEdge, MiniMap, Controls } from 'react-flow-renderer';
+import ReactFlow, {
+  removeElements, addEdge, MiniMap, Controls, Handle, Position,
+  getBezierPath,
+  getEdgeCenter,
+  getMarkerEnd,
+} from 'react-flow-renderer';
 
 const flowStyles = { height: 500 };
 const onLoad = (reactFlowInstance: any) => reactFlowInstance.fitView();
 
+const onElementClick = (event: any, element: any) => console.log('click', element);
 const onNodeMouseEnter = (event: any, node: any) => console.log('mouse enter:', node);
 const onNodeMouseMove = (event: any, node: any) => console.log('mouse move:', node);
 const onNodeMouseLeave = (event: any, node: any) => console.log('mouse leave:', node);
@@ -69,6 +75,9 @@ const initialElements: any = [
     sourcePosition: 'right',
     targetPosition: 'left',
     data: { label: 'Node 8' },
+    type: 'selectorNode',
+    // data: { onChange: onChange, color: initBgColor },
+    style: { border: '1px solid #777', padding: 10 },
     position: { x: 750, y: 300 },
   },
 
@@ -92,6 +101,7 @@ const initialElements: any = [
     type: 'smoothstep',
     target: 'horizontal-4',
     label: 'edge label',
+    arrowHeadType: 'arrowclosed',
   },
   {
     id: 'horizontal-e3-5',
@@ -117,17 +127,59 @@ const initialElements: any = [
   {
     id: 'horizontal-e6-8',
     source: 'horizontal-6',
-    type: 'smoothstep',
+    type: 'buttonedge',
     target: 'horizontal-8',
     animated: true,
   },
 ];
 
+const newNode = {
+  id: 'newNode',
+  // sourcePosition: 'right',
+  // targetPosition: 'left',
+  data: { label: 'New Node' },
+  position: { x: 800, y: 350 },
+}
+const newEdge = {
+  id: 'horizontal-e7-n',
+  source: 'horizontal-6',
+  type: 'smoothstep',
+  target: 'newNode',
+  animated: true,
+}
+
+
 const App = () => {
-  const [elements, setElements] = useState(initialElements);
+  const [elements, setElements] = useState<any[]>(initialElements);
   const onElementsRemove = (elementsToRemove: any) =>
     setElements((els: any) => removeElements(elementsToRemove, els));
   const onConnect = (params: any) => setElements((els: any) => addEdge(params, els));
+  const deleteNode = () => {
+    setElements((elms: any[]) => {
+      elms = elms.filter((u: any) => u.id !== newNode.id
+      )
+      return [...elms]
+    });
+  };
+  const addNode = () => {
+    setElements((elms: any[]) => {
+      return [...elms, newNode]
+    }
+    );
+  };
+  const addEdgeCustom = () => {
+    setElements((elms: any[]) => {
+      return [...elms, newEdge]
+    }
+    );
+  };
+  const deleteEdgeCustom = () => {
+    setElements((elms: any[]) => {
+      elms = elms.filter((u: any) => u.id !== newEdge.id
+      )
+      return [...elms]
+    });
+  };
   const changeClassName = () => {
     setElements((elms: any) =>
       elms.map((el: any) => {
@@ -143,6 +195,7 @@ const App = () => {
   return (
     <ReactFlow
       elements={elements}
+      onElementClick={onElementClick}
       onElementsRemove={onElementsRemove}
       onConnect={onConnect}
       onLoad={onLoad}
@@ -151,18 +204,152 @@ const App = () => {
       onNodeMouseMove={onNodeMouseMove}
       onNodeMouseLeave={onNodeMouseLeave}
       onNodeContextMenu={onNodeContextMenu}
+      nodeTypes={nodeTypes}
+      edgeTypes={edgeTypes}
       style={flowStyles}
     >
       <MiniMap />
       <Controls />
-      <button
+      {/* <button
         onClick={changeClassName}
         style={{ position: 'absolute', right: 10, top: 30, zIndex: 4 }}
       >
         change class name
+      </button> */}
+      <button
+        onClick={addNode}
+        style={{ position: 'absolute', right: 10, top: 30, zIndex: 4 }}
+      >
+        add node
+      </button>
+      <button
+        onClick={deleteNode}
+        style={{ position: 'absolute', right: 10, top: 60, zIndex: 4 }}
+      >
+        delete node
+      </button>
+      <button
+        onClick={addEdgeCustom}
+        style={{ position: 'absolute', right: 10, top: 90, zIndex: 4 }}
+      >
+        add edge
+      </button>
+      <button
+        onClick={deleteEdgeCustom}
+        style={{ position: 'absolute', right: 10, top: 120, zIndex: 4 }}
+      >
+        delete edge
       </button>
     </ReactFlow>
   );
 };
 
 export default App;
+
+const ColorSelectorNode = (({ data, isConnectable }: { data: any, isConnectable: boolean }) => {
+  return (
+    <>
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{ background: '#555' }}
+        onConnect={(params: any) => console.log('handle onConnect', params)}
+        isConnectable={isConnectable}
+      />
+      <div>
+        Custom Color Picker Node: <strong>{data.color}</strong>
+      </div>
+      <input
+        className="nodrag"
+        type="color"
+        onChange={data.onChange}
+        defaultValue={data.color}
+      />
+      <Handle
+        type="source"
+        position={Position.Right}
+        id="a"
+        style={{ top: 10, background: '#555' }}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="source"
+        position={Position.Right}
+        id="b"
+        style={{ bottom: 10, top: 'auto', background: '#555' }}
+        isConnectable={isConnectable}
+      />
+    </>
+  );
+});
+
+const nodeTypes: any = {
+  selectorNode: ColorSelectorNode,
+};
+const foreignObjectSize = 40;
+
+const onEdgeClick = (evt: any, id: any) => {
+  evt.stopPropagation();
+  alert(`remove ${id}`);
+};
+const ButtonEdge = ({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  data,
+  arrowHeadType,
+  markerEndId,
+}: any) => {
+  const edgePath = getBezierPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+  const markerEnd = getMarkerEnd(arrowHeadType, markerEndId);
+  const [edgeCenterX, edgeCenterY] = getEdgeCenter({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+  });
+
+  return (
+    <>
+      <path
+        id={id}
+        style={style}
+        className="react-flow__edge-path"
+        d={edgePath}
+        markerEnd={markerEnd}
+      />
+      <foreignObject
+        width={foreignObjectSize}
+        height={foreignObjectSize}
+        x={edgeCenterX - foreignObjectSize / 2}
+        y={edgeCenterY - foreignObjectSize / 2}
+        className="edgebutton-foreignobject"
+        requiredExtensions="http://www.w3.org/1999/xhtml"
+      >
+        <body>
+          <button
+            className="edgebutton"
+            onClick={(event) => onEdgeClick(event, id)}
+          >
+            ×
+          </button>
+        </body>
+      </foreignObject>
+    </>
+  );
+}
+const edgeTypes = {
+  buttonedge: ButtonEdge,
+};
